acea878d34e1797a5af98c147b4a12646d8c3ea5
twe <tobias.wessels@ongoingmedia.de>
2021-06-30 11:26:10 +0200

    Update staticfiles storage config

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -140,5 +140,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
